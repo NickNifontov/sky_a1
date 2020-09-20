eadad438dab03d11c256d010a8f4b144cc562f78
RadaR <RadaR@192.168.0.111>
2020-09-21 01:17:15 +0300

    update

--- Core/Src/iwdg.c
+++ Core/Src/iwdg.c
@@ -31,7 +31,7 @@ void MX_IWDG_Init(void)
   LL_IWDG_Enable(IWDG);
   LL_IWDG_EnableWriteAccess(IWDG);
   LL_IWDG_SetPrescaler(IWDG, LL_IWDG_PRESCALER_128);
-  LL_IWDG_SetReloadCounter(IWDG, 4095);
+  LL_IWDG_SetReloadCounter(IWDG, 4095+2);
   while (LL_IWDG_IsReady(IWDG) != 1)
   {
   }
